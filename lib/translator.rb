607ec54fc0abfaef1868238c2b7995a1e4db0835
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 19:20:42 -0400

    "Feat: Update attributes for clarity, add format method"

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,13 @@ require_relative '../lib/input_output'
 require 'CSV'
 
 class Translator
-  attr_reader :dictionary, :csv_hash
+  attr_reader :dictionary, :dictionary_hash, :translated_array
 
   def initialize(dictionary)
     @dictionary = dictionary
-    @csv_hash = {}
+    @dictionary_hash = {}
+    @translated_array = []
+
   end
 
   def self.from_csv(path)
@@ -16,14 +18,19 @@ class Translator
 
   def update_hash#move to module to keep in init?
     @dictionary.each do |row|
-      @csv_hash[row[:character]] = row[:braille]
+      @dictionary_hash[row[:character]] = row[:braille]
     end
   end
 
   def translate(text_to_translate)
-    array = []
-    array << @csv_hash[text_to_translate][0..1]
-    array << @csv_hash[text_to_translate][2..3]
-    array << @csv_hash[text_to_translate][4..5]
+    @translated_array << @dictionary_hash[text_to_translate][0..1]
+    @translated_array << @dictionary_hash[text_to_translate][2..3]
+    @translated_array << @dictionary_hash[text_to_translate][4..5]
+  end
+
+  def format
+    @translated_array.map do |element|
+      element << '\n'
+    end
   end
 end
