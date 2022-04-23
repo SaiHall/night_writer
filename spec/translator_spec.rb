3313e2484e76f2cfb56320edcb4bc269017c0045
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 16:42:51 -0400

    "Fix: Add missing argument to new instance of translator"

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -3,7 +3,7 @@ require 'Rspec'
 
 describe Translator do
   before(:each) do
-    @translator = Translator.new
+    @translator = Translator.new('.docs/dictionary.csv')
   end
   it 'exists' do
     expect(@translator).to be_an_instance_of(Translator)
