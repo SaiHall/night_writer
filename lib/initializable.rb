8e3dfc684dde3d230431d89dd2215bcbc40eca4f
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 19:59:08 -0400

    "Feat: Fill in module with methods wanted for initialize"

--- lib/initializable.rb
+++ lib/initializable.rb
@@ -1,3 +1,18 @@
-module Initializeable
 
+module Initializable
+
+  def read_incoming
+    incoming = File.open(@incoming_file, "r")
+    incoming_text = (incoming.read).chomp
+    incoming.close
+    return incoming_text
+  end
+
+  def update_hash
+    dictionary_hash = {}
+    @dictionary.each do |row|
+      dictionary_hash[row[:character]] = row[:braille]
+    end
+    return dictionary_hash
+  end
 end
