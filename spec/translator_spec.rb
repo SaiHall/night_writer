0735daf186c725f9277183cf49ae5f4702b3504f
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 16:34:51 -0400

    "Fix: Add @ for before each block"

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -3,9 +3,9 @@ require 'Rspec'
 
 describe Translator do
   before(:each) do
-    translator = Translator.new
+    @translator = Translator.new
   end
   it 'exists' do
-    expect(translator).to be_an_instance_of(Translator)
+    expect(@translator).to be_an_instance_of(Translator)
   end
 end
