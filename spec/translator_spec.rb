bfde0598f44c20b1cf3df228fd057c818eff8e24
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 17:23:58 -0400

    "Test: Add expected hash and update hash test"

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -14,4 +14,36 @@ describe Translator do
     expect(@translator.dictionary).to be_an_instance_of(CSV::Table)
     expect(@translator.csv_hash).to eq({})
   end
+
+  it 'can update the CSV hash based off dictionary' do
+    @translator.update_hash
+    expected = {" " => "......",
+       "a" => "0.....",
+       "b" => "0.0...",
+       "c" => "00....",
+       "d" => "00.0..",
+       "e" => "0..0..",
+       "f" => "000...",
+       "g" => "0000..",
+       "h" => "0.00..",
+       "i" => ".00...",
+       "j" => ".000..",
+       "k" => "0...0.",
+       "l" => "0.0.0.",
+       "m" => "00..0.",
+       "n" => "00.00.",
+       "o" => "0..00.",
+       "p" => "000.0.",
+       "q" => "00000.",
+       "r" => "0.000.",
+       "s" => ".00.0.",
+       "t" => ".0000.",
+       "u" => ".0..00",
+       "v" => "0.0.00",
+       "w" => ".000.0",
+       "x" => "00..00",
+       "y" => "00.000",
+       "z" => "0..000"}
+    expect(@translator.csv_hash).to eq(expected)
+  end
 end
