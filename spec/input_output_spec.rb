865f6ef2224addbdab0be08cee15878fef997031
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 12:59:37 -0400

    "Test: Add readable attributes test"

--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -2,8 +2,16 @@ require_relative '../lib/input_output'
 require 'Rspec'
 
 describe InputOutput do
-  it 'exists' do
-    input_output = InputOutput.new('message.txt', 'braille.txt')
-    expect(input_output).to be_an_instance_of(InputOutput)
+  before(:each) do
+    @input_output = InputOutput.new('message.txt', 'braille.txt')
   end
+  it 'exists' do
+    expect(@input_output).to be_an_instance_of(InputOutput)
+  end
+
+  it 'has readable attributes' do
+    expect(@input_output.incoming_text).to be_a(File)
+    expect(@input_output.outgoing_text).to be_a(File)
+  end
+
 end
