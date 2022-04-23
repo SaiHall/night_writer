5624b57c5f55fddb0a475263f4dda2f63ad1ee84
Sarah Hall <swatson1994@yahoo.com>
2022-04-23 17:14:59 -0400

    "Test: Adjust tests for CSV file"

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,15 +1,16 @@
 require_relative '../lib/translator'
 require 'Rspec'
+require 'CSV'
 
 describe Translator do
   before(:each) do
-    @translator = Translator.new('.docs/dictionary.csv')
+    @translator = Translator.from_csv('./docs/dictionary.csv')
   end
   it 'exists' do
     expect(@translator).to be_an_instance_of(Translator)
   end
 
   it 'has readable attributes' do
-    expect(@translator.dictionary_path).to eq('.docs/dictionary.csv')
+    expect(@translator.dictionary).to be_an_instance_of(CSV::Table)
   end
 end
