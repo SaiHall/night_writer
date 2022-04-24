f648022afaf423eb9f1cb84553732d4e95eb38ab
Sarah Hall <swatson1994@yahoo.com>
2022-04-24 19:12:22 -0400

    "Feat: Add incoming text attribute"

--- lib/input_output_read.rb
+++ lib/input_output_read.rb
@@ -1,10 +1,15 @@
-class InputOutputRead
+require_relative './initializable'
 
-  attr_reader :incoming_file, :outgoing_file
+
+class InputOutputRead
+  include Initializable
+
+  attr_reader :incoming_file, :outgoing_file, :incoming_text
 
   def initialize(user_input1, user_input2)
     @incoming_file = user_input1
     @outgoing_file = user_input2
+    @incoming_text = read_incoming
   end
 
 end
