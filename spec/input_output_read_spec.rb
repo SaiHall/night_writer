3c51632e27af2e0fa8b5916f80712c9e2335273c
Sarah Hall <swatson1994@yahoo.com>
2022-04-24 18:59:20 -0400

    "Test: Fill out attribute tests for io read"

--- spec/input_output_read_spec.rb
+++ spec/input_output_read_spec.rb
@@ -3,10 +3,16 @@ require 'Rspec'
 require 'CSV'
 
 describe InputOutputRead do
-  it 'exists' do
     before(:each) do
         @io_read = InputOutputRead.new('braille.txt', 'original_message.txt')
     end
-    expect(@io_read).to be_an_instance_of(InputOutputRead)
-  end
+
+    it 'exists' do
+      expect(@io_read).to be_an_instance_of(InputOutputRead)
+    end
+    it 'has readable attributes' do
+      expect(@io_read.incoming_file).to eq('braille.txt')
+      expect(@io_read.outgoing_file).to eq('original_message.txt')
+    end
+
 end
