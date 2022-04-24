f9dc0e0a2bdd3a260dec00258176b746eb605d35
Sarah Hall <swatson1994@yahoo.com>
2022-04-24 19:54:42 -0400

    "Test: Add test for braille to eng dictionary"

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -51,4 +51,40 @@ describe Translator do
     @translator.translate('a')
     expect(@translator.format).to eq(["0.\n", "..\n", "..\n"])
   end
+
+  context 'Braille dictionary update' do
+    before(:each) do
+      @translator = Translator.from_csv('./docs/eng_to_braille_dict.csv')
+    end
+    it 'has a separate dictionary for braille to english' do
+      expected = {"0....."=>"a",
+        "0.0..."=>"b",
+        "00...."=>"c",
+        "00.0.."=>"d",
+        "0..0.."=>"e",
+        "000..."=>"f",
+        "0000.."=>"g",
+        "0.00.."=>"h",
+        ".00..."=>"i",
+        ".000.."=>"j",
+        "0...0."=>"k",
+        "0.0.0."=>"l",
+        "00..0."=>"m",
+        "00.00."=>"n",
+        "0..00."=>"o",
+        "000.0."=>"p",
+        "00000."=>"q",
+        "0.000."=>"r",
+        ".00.0."=>"s",
+        ".0000."=>"t",
+        ".0..00"=>"u",
+        "0.0.00"=>"v",
+        ".000.0"=>"w",
+        "00..00"=>"x",
+        "00.000"=>"y",
+        "0..000"=>"z",
+        "......"=>" "}
+      expect(@translator.braille_dict_hash).to eq(expected)
+    end
+  end
 end
