6b4cba5bc48384c5e73bbfdf5742c8c6f4be8bfa
Sarah Hall <swatson1994@yahoo.com>
2022-04-24 19:16:23 -0400

    "Test: Add context and braille char count test"

--- spec/input_output_read_spec.rb
+++ spec/input_output_read_spec.rb
@@ -3,6 +3,7 @@ require 'Rspec'
 require 'CSV'
 
 describe InputOutputRead do
+  context 'set up of new io class' do
     before(:each) do
         @io_read = InputOutputRead.new('braille.txt', 'original_message.txt')
     end
@@ -18,5 +19,11 @@ describe InputOutputRead do
     it 'Can open and read the contents of attribute file' do
         expect(@io_read.incoming_text).to eq(".00.0....00.0.0.00..0.0.0.00\n0000.0..00.00.0..0..0..0...0\n0...........0.0.00........0.")
     end
+  end
 
+  context 'Counting characters from braille' do
+    it 'can count braille characters' do
+      expect(@io_read.braille_count).to eq(14)
+    end
+  end
 end
