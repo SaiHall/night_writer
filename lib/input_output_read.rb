5d5ecf51a37a9e57b2159075c328750c1ee3bf15
Sarah Hall <swatson1994@yahoo.com>
2022-04-24 20:07:17 -0400

    "Feat: Add translator to io read"

--- lib/input_output_read.rb
+++ lib/input_output_read.rb
@@ -1,15 +1,17 @@
+require_relative './translator'
 require_relative './initializable'
-
 
 class InputOutputRead
   include Initializable
 
-  attr_reader :incoming_file, :outgoing_file, :incoming_text
+  attr_reader :incoming_file, :outgoing_file, :incoming_text, :translator
 
   def initialize(user_input1, user_input2)
     @incoming_file = user_input1
     @outgoing_file = user_input2
     @incoming_text = read_incoming
+    @translator = Translator.from_csv('./docs/eng_to_braille_dict.csv')
+
   end
 
   def braille_count
